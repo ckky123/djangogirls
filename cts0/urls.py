13553c6311186059e03b429c4ac53d203ca9d72a
Wendy <ckky123@gmail.com>
2016-07-15 00:18:07 +0800

    uptdate

--- cts0/urls.py
+++ cts0/urls.py
@@ -2,9 +2,7 @@ from django.conf.urls import patterns, include, url
 from . import views
 
 urlpatterns = patterns('',
-
-    url(r'^$', views.index),
-    url(r'^add/(\d+)/(\d+)/$', views.add, name='add'),
+    url(r'^$', views.index, name='index'),
     url(r'^QueryOneWeek/$', views.QueryOneWeek, name='QueryOneWeek'),
     
 )
--- cts0/views.py
+++ cts0/views.py
@@ -8,16 +8,11 @@ import pymysql
 import time
 # Create your views here.
 
+
 def index(request):
     return render(request, 'cts0/index.html')
 
-#def add(request):
-    #a = request.GET['from']
-   # b = request.GET['to']
-    #c = a+b
-    #return HttpResponse(str(c))
-    #return HttpResponse("aaaaaaa")
-    
+
 #def index(request):
 #    return HttpResponse(u"欢迎光临 自强学堂!")
 
